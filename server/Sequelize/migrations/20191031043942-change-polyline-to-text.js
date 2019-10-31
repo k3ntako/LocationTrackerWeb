394f83d566f9500cbd3a68fe5b832243693b2d07
k3ntako <kentarokaneki@gmail.com>
2019-10-31 01:01:57 -0400

    Change polyline to TEXT

---
+++ server/Sequelize/migrations/20191031043942-change-polyline-to-text.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn(
+      'Runs',
+      'polyline',
+      {
+        type: Sequelize.TEXT,
+      }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn(
+    'Runs',
+      'polyline',
+      {
+        type: Sequelize.STRING,
+      }
+    )
+  }
+};
